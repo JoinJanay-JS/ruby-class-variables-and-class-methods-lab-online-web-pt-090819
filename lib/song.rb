4f6379836d496afc5793608f78b4ed448ba223d4
Janay Bailey <tjanay87@gmail.com>
2019-10-01 19:27:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,7 +21,9 @@ class Song
     genre_count
   end
   @@artists = []
-  
+  def self.genre_count
+    @@genres.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
+  end 
   
   def self.artist_count
     @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
